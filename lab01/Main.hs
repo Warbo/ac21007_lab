8b6eb31790ed701fbbd6f894f4dc3c30b7546415
Chris Warburton <chriswarbo@gmail.com>
2016-01-18 17:15:46 +0000

    Solutions

--- lab01/Main.hs
+++ lab01/Main.hs
@@ -10,7 +10,7 @@ module Main where
 
 -- | Simple function to create a hello message.
 hello :: String -> String
-hello s = "Hello " ++ s ++ "!"
+hello s = "Goodbye " ++ s ++ "!"
 
 -- | Entry point of a program
 main :: IO ()
@@ -24,22 +24,33 @@ main = putStrLn (hello "World")
 -- | myAnd returns True only if both of the inputs are True.
 --
 -- myAnd :: Bool -> Bool -> Bool,
+myAnd True True = True
+myAnd x    y    = False
 
 -- | myOr returns False only if both of the inputs are False.
 --
 -- myOr :: Bool -> Bool -> Bool
+
+myOr False False = False
+myOr x     y     = True
 
 -- | myImply x y should return False only if x is True and y is False, for the
 --   other cases it returns True
 --
 -- myImply :: Bool -> Bool -> Bool
 
+myImply True False = False
+myImply x    y     = True
+
 -- | myXor is called Exclusive disjunction (exclusive or), is a logical operation
 --   that outputs True whenever both inputs differ (one is True, the other is False)
 --
 -- myXor :: Bool -> Bool -> Bool.
 
-
+myXor True True   = False
+myXor True False  = True
+myXor False True  = True
+myXor False False = False
 
 -- | Define a function that converts boolean value to its coresponding text description,
 --   e. g. 
@@ -48,5 +59,5 @@ main = putStrLn (hello "World")
 --
 -- boolToString :: Bool -> String
 
-
-
+boolToString True  = "true"
+boolToString False = "false"
